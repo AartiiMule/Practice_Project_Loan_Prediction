f8832ac3f2593aeeded5e7bccbe779c2e97ae8fd
AartiiMule <muleaarti0505@gmail.com>
2024-03-14 15:08:04 +0530

    Configuration File

---
+++ config.py
@@ -0,0 +1,13 @@
+
+
+
+
+import os
+
+port_number = 1004  
+
+MODEL_FILE_PATH = os.path.join("project_app","rf_scaled_model.pkl")
+
+JSON_FILE_PATH = os.path.join ("project_app","json_file.json")
+
+SCALED_FILE_PATH = os.path.join("project_app","scaled_model.pkl")
